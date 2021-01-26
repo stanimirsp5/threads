8457715dff77b11b18cee4c5f5c5d5aee76103b8
Stan <stanimir.sp5@gmail.com>
2021-01-26 22:23:53 +0200

    java thread with UI swing

---
+++ src/threads/Ticker.java
@@ -0,0 +1,90 @@
+package threads;
+
+//: CounterPT.java
+// If you separate your thread from the main
+// class, you can have as many threads as you want.
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Ticker extends Thread {
+    private Button b = new Button("Toggle");
+    private TextField t = new TextField(10);
+    private int count = 0;
+    private boolean runFlag = true;
+    public Ticker(Container c) {
+        b.addActionListener(new ToggleL());
+        JPanel p = new JPanel();
+        p.add(t); p.add(b); c.add(p);
+    }
+    class ToggleL implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            runFlag = !runFlag;
+        }
+    }
+    public void run() {
+        while (true) {
+            if(runFlag)
+                t.setText(Integer.toString(count++));
+            try {
+                sleep(100);
+            } catch (InterruptedException e){}
+        }
+    }
+    public void stp() {
+        runFlag = false;
+    }
+    public void restart() {
+        runFlag = true;
+    }
+}
+
+class CounterPT extends JPanel {
+    private Button start = new Button("Start");
+    private Button stop = new Button("Stop");
+    private Button restart = new Button("Restart");
+    private boolean started = false;
+    private Ticker[] s;
+    private int size;
+    public void init() {
+        this.setLayout(new FlowLayout());
+        s = new Ticker[size];
+        for(int i = 0; i < s.length; i++)
+            s[i] = new Ticker(this);
+        start.addActionListener(new StartL());
+        add(start);
+        stop.addActionListener(new StopL());
+        add(stop);
+        restart.addActionListener(new RestartL());
+        add(restart);
+    }
+    class StartL implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if(!started) {
+                started = true;
+                for(int i = 0; i < s.length; i++) s[i].start();
+            }
+        }
+    }
+    class StopL implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            for(int i=0;i<s.length;i++) s[i].stp();
+        }
+    }
+    class RestartL implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            for(int i=0; i< s.length; i++) s[i].restart();
+        }
+    }
+    public static void main(String[] args) {
+        CounterPT cnt = new CounterPT();
+
+        cnt.size = (args.length == 0 ? 5 : Integer.parseInt(args[0]));
+        JFrame aFrame = new JFrame("CounterPT");
+        aFrame.add(cnt);
+        aFrame.setSize(200*(1+cnt.size/10), 500);
+        aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cnt.init();
+        aFrame.setVisible(true);
+    }
+}
--- src/threads/threadsExercises/OneThreadCounter.java
+++ src/threads/threadsExercises/OneThreadCounter.java
@@ -1,15 +1,42 @@
 package threads.threadsExercises;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
-
+//https://www.javatpoint.com/java-jtextfield
 public class OneThreadCounter implements Runnable{
     public volatile boolean isRunning = true;
     public int count;
-    public static void main(String[] args){
-        OneThreadCounter o = new OneThreadCounter();
-        o.startThread();
+    static JTextField textField = new JTextField();
 
+    public static void main(String[] args) throws InterruptedException {
 
+        JFrame f=new JFrame();//creating instance of JFrame
+
+        JButton b=new JButton("start");//creating instance of JButton
+        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+
+        textField.setBounds(50,300, 100,30);
+
+        f.add(b);//adding button in JFrame
+        f.add(textField);
+
+        f.setSize(400,500);//400 width and 500 height
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
+
+        b.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                OneThreadCounter o = new OneThreadCounter();
+                o.startThread();
+            }
+        });
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(100);
+            textField.setText(String.valueOf(i));
+
+        }
     }
 
     public void startThread(){
@@ -38,6 +65,7 @@ public class OneThreadCounter implements Runnable{
             }
 
             if (isRunning){
+                textField.setText(count++ + " " + Thread.currentThread().getName() + " ");
                 System.out.print(count++ + " " + Thread.currentThread().getName() + " ");
             }
 
