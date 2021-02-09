ce04996db9807da51296c202416646f5fdeb6b90
stanimirsp5 <stanimir.sp5@gmail.com>
2021-02-09 18:17:59 +0200

    two bridges exercise

--- src/threads/Synchronization/exercise/TwoBridges.java
+++ src/threads/Synchronization/exercise/TwoBridges.java
@@ -1,60 +1,118 @@
 package threads.Synchronization.exercise;
 
 public class TwoBridges {
-    public static final int NUM_THREADS = 4;
+    public static final int NUM_THREADS = 40;
 
     public static void main(String[] args){
         MainBridge leftSideBridge = new MainBridge();
         MainBridge rightSideBridge = new MainBridge();
 
         for (int i = 0; i < NUM_THREADS; i++) {
-            Direction direction = Math.random() > 0.5 ? Direction.LEFT : Direction.RIGHT;
-            new Car(leftSideBridge,rightSideBridge,direction);
+            CarDirection direction = Math.random() > 0.5 ? CarDirection.LEFT : CarDirection.RIGHT;
+            new Car(leftSideBridge,rightSideBridge,direction);//.start();
         }
 
     }
 }
 
-class Car implements Runnable{
+class Car extends Thread{
     MainBridge leftB, rightB;
-    Direction direction;
-
-    public Car(MainBridge leftB, MainBridge rightB, Direction direction) {
+    CarDirection direction;
+    int bridgeNum = 0;
+    String name;
+    public Car(MainBridge leftB, MainBridge rightB, CarDirection direction) {
         this.leftB = leftB;
         this.rightB = rightB;
         this.direction = direction;
+        name = direction.name() + " " + Thread.currentThread().getName();
+        this.start(); //difference?
+        //super.setName(name);
     }
-// need way to indicate whe one thread is left one bridge so it can take on another
+
     @Override
     public void run() {
-        if (direction == Direction.LEFT) {
-            leftB.takeBridge();
+        name = direction.name() + " " + Thread.currentThread().getName();
+
+        if (direction == CarDirection.LEFT) {
+            bridgeNum = 1;
+
+            leftB.takeBridge(bridgeNum,name, direction);
         } else {
-            rightB.takeBridge();
+            bridgeNum = 2;
+
+            rightB.takeBridge(bridgeNum,name, direction);
         }
 
         try {
-            Thread.sleep(50);
+            Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        if (direction == Direction.LEFT) {
-            leftB.leaveBridge();
+        if (direction == CarDirection.LEFT) {
+
+            leftB.leaveBridge(bridgeNum,name, direction);
         } else {
-            rightB.leaveBridge();
+
+            rightB.leaveBridge(bridgeNum,name, direction);
         }
 
+        // second bridge
+
+        if (direction == CarDirection.LEFT) {
+            bridgeNum = 2;
+
+            rightB.takeBridge(bridgeNum,name, direction);
+        } else {
+            bridgeNum = 1;
+
+            leftB.takeBridge(bridgeNum,name, direction);
+        }
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (direction == CarDirection.LEFT) {
+            rightB.leaveBridge(bridgeNum,name, direction);
+        } else {
+            leftB.leaveBridge(bridgeNum,name, direction);
+        }
     }
 }
-
+//r > 0 > l
 class MainBridge{
-    public void takeBridge(){
+    int carsOnTheBridge;
+    synchronized public void takeBridge(int bridgeNum,String name, CarDirection direction){
+        while ( direction == CarDirection.LEFT && carsOnTheBridge > 0 ||
+                direction == CarDirection.RIGHT && carsOnTheBridge < 0){
+            try {
+                System.out.println( name + " is waiting for bridge " + bridgeNum );
 
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if(direction == CarDirection.LEFT){
+            carsOnTheBridge--;
+        }else {
+            carsOnTheBridge++;
+        }
+        System.out.println( name + " take bridge " + bridgeNum );
     }
 
-    public void leaveBridge(){
-
+    synchronized public void leaveBridge(int bridgeNum,String name, CarDirection direction){
+        
+        System.out.println( name + " left bridge " + bridgeNum );
+        notifyAll();
+        if(direction == CarDirection.LEFT){
+            carsOnTheBridge++;
+        }else {
+            carsOnTheBridge--;
+        }
     }
 
 }
