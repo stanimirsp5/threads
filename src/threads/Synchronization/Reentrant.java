c0fd386a83da0aa5be415f9dd870e6a32341c74a
stanimirsp5 <stanimir.sp5@gmail.com>
2021-02-10 18:17:50 +0200

    reenterant exercise

--- src/threads/Synchronization/Reentrant.java
+++ src/threads/Synchronization/Reentrant.java
@@ -1,20 +1,74 @@
 package threads.Synchronization;
 
 public class Reentrant {
-    public static void main(String a[]){
-        Reentrant r = new Reentrant();
-        r.a();
+    public static void main(String a[]) throws InterruptedException {
+        //        Reentrant r = new Reentrant();
+        //        r.a();
         // try to access to nested synchronized methods
+        Counter counter = new Counter();
+        for (int i = 0; i < 5; i++) {
+            Worker worker = new Worker(counter);
+            Thread thread = new Thread(worker);
+            thread.start();
+        }
+        Thread.currentThread().sleep(1000);
+        System.out.println("Total "+counter.getNum());
     }
-    public synchronized void a() {
-        b();
-        System.out.println("here I am, in a()");
+//    public synchronized void a() {
+//        b();
+//        System.out.println("here I am, in a()");
+//    }
+//    public void test() {
+//        b();
+//        System.out.println("here I am, in a()");
+//    }
+//    public synchronized void b() {
+//        System.out.println("here I am, in b()");
+//    }
+}
+
+class Worker implements Runnable{
+    Counter counter;
+
+    public Worker(Counter counter){
+        this.counter = counter;
     }
-    public void test() {
-        b();
-        System.out.println("here I am, in a()");
+
+
+    @Override
+    public void run() {
+
+        counter.add();
+
+
+
     }
-    public synchronized void b() {
-        System.out.println("here I am, in b()");
+}
+
+class Counter{
+    int number = 0;
+    public synchronized void add(){
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        number++;
+        System.out.println(Thread.currentThread().getName() +" Add "+number);
+
+        extract();
+    }
+    public synchronized void extract(){
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        number--;
+        System.out.println(Thread.currentThread().getName() + " Extract "+number);
+
+    }
+    public int getNum(){
+        return number;
     }
 }
