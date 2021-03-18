6ce86376e810e20c7abdc73f7f20ec53dd83df50
stanimirsp5 <stanimir.sp5@gmail.com>
2021-03-18 18:02:21 +0200

    sockets exercise

--- src/JavaWeb/Sockets/exersices/ClientWriter.java
+++ src/JavaWeb/Sockets/exersices/ClientWriter.java
@@ -1,11 +1,12 @@
 package JavaWeb.Sockets.exersices;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 // https://www.edureka.co/blog/socket-programming-in-java/#:~:text=Socket%20programming%20in%20Java%20is,a%20client%20and%20a%20server.
 
 public class ClientWriter {
-    public static final int PORT = 1717;
+    public static final int PORT = 5000;
     public static final String IP = "192.168.0.18";
 
     public static void main(String[] args) throws IOException {
@@ -13,25 +14,27 @@ public class ClientWriter {
     }
 
     public static void runClient() {
+        System.out.println("Client started");
         try (
                 Socket socket = new Socket(IP, PORT);
 
                 BufferedReader input = new BufferedReader(new InputStreamReader(System.in)); // takes input from terminal
-                PrintWriter output = new PrintWriter(socket.getOutputStream()); // sends input to server
+                PrintWriter output = new PrintWriter(socket.getOutputStream()); // sends input to server. Send the output through the socket.
         ) {
             String line = "";
-            while (!line.equals("Over")) {
-                line = input.readLine();
-                output.write(line);
+            while ((line = input.readLine()) != null) {
+               // System.out.println((line = input.readLine()) != null);
+                output.println(line);
             }
+//            while (!line.equals("Over")) {
+//                line = input.readLine();
+//                output.write(line);
+//            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void runServer() {
 
-
-    }
 }
--- src/JavaWeb/Sockets/exersices/ServerReader.java
+++ src/JavaWeb/Sockets/exersices/ServerReader.java
@@ -1,17 +1,38 @@
 package JavaWeb.Sockets.exersices;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
 import java.net.Socket;
 // https://www.edureka.co/blog/socket-programming-in-java/#:~:text=Socket%20programming%20in%20Java%20is,a%20client%20and%20a%20server.
 public class ServerReader {
-    public static final int PORT = 1717;
+    public static final int PORT = 5000;
     public static void main(String[] args) throws IOException {
+        runServer();
 
     }
 
-    public static void Server(){
+    public static void runServer() throws IOException {
+        System.out.println("Server started");
 
-        Socket server = new Socket();
+        //getOutputStream() method is used to send the output through the socket.
+        try(
+
+                ServerSocket server = new ServerSocket(PORT);
+                Socket socket = server.accept();
+                // takes input from the client socket
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ){
+            String line = "";
+            while ((line = input.readLine()) != null)
+            {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
