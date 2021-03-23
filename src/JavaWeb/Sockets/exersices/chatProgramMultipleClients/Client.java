671e1acfb0b2f873097a633a939114660c4320e5
stanimirsp5 <stanimir.sp5@gmail.com>
2021-03-23 09:49:57 +0200

    multiclient exercise

--- src/JavaWeb/Sockets/exersices/chatProgramMultipleClients/Client.java
+++ src/JavaWeb/Sockets/exersices/chatProgramMultipleClients/Client.java
@@ -1,27 +1,34 @@
 package JavaWeb.Sockets.exersices.chatProgramMultipleClients;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
+// https://gyawaliamit.medium.com/multi-client-chat-server-using-sockets-and-threads-in-java-2d0b64cad4a7 - multiclient
+// https://stackoverflow.com/questions/10131377/socket-programming-multiple-client-to-one-server
 public class Client {
     public static final int PORT = 4444;
-    public static final String IP = "192.168.100.17";// "127.0.0.1";
+    public static final String IP = "127.0.0.1";// "127.0.0.1"; 192.168.100.17
 
     public static void main(String[] args) {
 
         try(
 
-                Socket socket = new Socket(IP, PORT);
-                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            Socket socket = new Socket(IP, PORT);
 
-                ){
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter writeToServer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true); // flush to send data immediately
+            // If you are writing through a network connection, you may want to call flush() so that the data gets sent through the network immediately.
+            BufferedReader messageFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
+        ){
+            String readUserInput;
+            while (true){
+                readUserInput = userInput.readLine();
+                writeToServer.println(readUserInput);
+
+                System.out.println(messageFromServer.readLine());
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/JavaWeb/Sockets/exersices/chatProgramMultipleClients/Server.java
+++ src/JavaWeb/Sockets/exersices/chatProgramMultipleClients/Server.java
@@ -1,4 +1,36 @@
 package JavaWeb.Sockets.exersices.chatProgramMultipleClients;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Server {
+    public static final int PORT = 4444;
+
+    public static void main(String[] args) {
+
+        try(
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            Socket client = serverSocket.accept();
+
+            BufferedReader readFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            PrintWriter writeToClient = new PrintWriter(client.getOutputStream(), true);
+        ){
+            String clientInput;
+            while ((clientInput = readFromClient.readLine()) !=null){
+
+                writeToClient.println("Response from server: " + clientInput);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
