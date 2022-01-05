81e0928c18bd895e345eafc13f758c7158000770
Stan <stanimir.sp5@gmail.com>
2022-01-06 00:40:58 +0200

    exam workshop 2 stopwatch start pause resume

--- src/ExamWorkshop/vechiclesExercises/CarWithDistance.java
+++ src/ExamWorkshop/vechiclesExercises/CarWithDistance.java
@@ -1,56 +1,32 @@
 package ExamWorkshop.vechiclesExercises;
-
 import java.util.concurrent.TimeUnit;
-
 public class CarWithDistance {
-
+// how to connect road length and car travel
     public static void main(String[] args) throws InterruptedException {
-//        Car car = new Car();
-//        Thread thread = new Thread(car);
-//        Thread.sleep(5000);
+        Car car = new Car(90);
+        Thread thread = new Thread(car);
+        Thread.sleep(500);
 //        Car car2 = new Car();
 //        Thread thread2 = new Thread(car2);
 //
-//        thread.start();
-//
-//        thread2.start();
-//
-        long start = System.nanoTime();
-        //System.out.println(start);
-        Thread.sleep(2000);
-        long end = System.nanoTime();
-        long elapsedTime = end - start;
-        //System.out.println(end);
-        System.out.println(elapsedTime);
-
-       long t = TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS);
-        System.out.println(t);
-        //stopwatch(4000);
-        //Thread.sleep(5000);
-        //stopwatch(1000);
-
+        thread.start();
     }
-
-
 }
-
 class Car implements Runnable{
-
-    private int velocity = 50; // m/s
+    private int velocity; // km/h
     private long time; // car travel time
     Stopwatch stopwatch;
 
-    public Car(){
+    public Car(int velocity){
+        this.velocity = 1000 * velocity / 3600; // convert to m/s
         stopwatch = new Stopwatch();
         stopwatch.start();
     }
 
     public double getPosition(){ // return position on road im meters
-
         double distance = getCarTime() * velocity;
         return distance;
     }
-
 
     public long getCarTime(){ // calculate car travel time in seconds
        return stopwatch.getTime();
@@ -59,6 +35,16 @@ class Car implements Runnable{
     @Override
     public void run() {
         System.out.println("position : " +getPosition());
+        while (getPosition() < 500){
+            try {
+                Thread.sleep(500);
+                //wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("position : " +getPosition());
+        }
+        System.out.println(getCarTime());
     }
 }
 
@@ -66,9 +52,9 @@ class Stopwatch{
     long start;
     long end;
 
-//    public Stopwatch(){
-//        start = System.nanoTime();
-//    }
+    long pauseStart;
+    long pauseEnd;
+
     public void start(){
         start = System.nanoTime();
     }
@@ -77,9 +63,19 @@ class Stopwatch{
         end = System.nanoTime();
     }
 
+    public void pause(){
+        pauseStart = System.nanoTime();
+    }
+
+    public void resume(){
+        pauseEnd = System.nanoTime();
+    }
+
     public long getTime(){
         end();
         long elapsedTime = end - start;
+        long elapsedPauseTime = pauseEnd - pauseStart;
+        pauseEnd -= elapsedPauseTime;
         long time = TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS);
         return time;
     }
